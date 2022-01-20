ab24883b094f493fa914836dbf9655e026e1e771
Stefan <s.madzarac19@gmail.com>
2022-01-20 22:32:24 +0000

    Part 3 - start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,36 @@ import React from "react";
 import Header from "./Header.jsx";
 import Note from "./Note.jsx";
 import Footer from "./Footer.jsx"
-import notes from "../notes.js"
+import CreateArea from "./CreateArea"
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map( eachNote => (
+      <CreateArea />
       <Note
-        key={eachNote.key}
-        title={eachNote.title}
-        content={eachNote.content} 
+        key={1}
+        title="Note Title"
+        content="Note Content" 
       />
-  ))}
       <Footer />
     </div>
   )
 }
 
 export default App;
+
+//CHALLENGE:
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
+
+//2. Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
+
+//This is the end result you're aiming for:
+//https://pogqj.csb.app/
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,12 +3,9 @@ import React from 'react';
 function Note(props) {
   return (
     <div className='note'>
-      <dt>
       <h1>{props.title}</h1>
-      </dt>
-      <dd>
       <p>{props.content}</p>
-      </dd>
+      <button>DELETE</button>
     </div>
   )
 }
